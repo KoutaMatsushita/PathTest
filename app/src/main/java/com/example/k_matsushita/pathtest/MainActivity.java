0e4657ee6bca8fe5ab80fcd974bc801ad683ed76
KoutaMatsushita <k-matsushita@nekojarashi.com>
2017-05-11 16:55:35 +0900

    Aaaaa

--- app/src/main/java/com/example/k_matsushita/pathtest/MainActivity.java
+++ app/src/main/java/com/example/k_matsushita/pathtest/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.k_matsushita.pathtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TestView view = (TestView)findViewById(R.id.view);
+        view.setImage(ImageSource.resource(R.drawable.img));
     }
 
-    
 }
--- app/src/main/java/com/example/k_matsushita/pathtest/TestView.java
+++ app/src/main/java/com/example/k_matsushita/pathtest/TestView.java
@@ -5,64 +5,60 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PointF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
 /**
  * Created by k-matsushita on 2017/05/11.
  */
 
-public class TestView extends View {
+public class TestView extends SubsamplingScaleImageView {
 
-    Path path;
-    Paint paint;
-
-    public TestView(Context context) {
-        super(context);
-    }
+    String svg = "M218.5 139.5l0 0l-1 0l-2 0l-1 0l-2 0l-1 0l-1 0l-2 0l-1 0l-1 0l-1 0l2 -1l4 -2l5 -2l4 -2l11 -4l10 -1l10 -3l8 0l11 0l6 0l6 0l5 2l5 7l3 7l2 8l2 8l1 10l0 9l0 10l-4 8l-6 9l-9 10l-9 6l-9 6l-4 1l-8 4l-13 4l-8 1l-5 0l-5 0l-5 -3l-2 -4l-2 -8l-1 -7l0 -10l0 -8l0 -9l6 -9l5 -7l7 -6l4 -1l11 -7l6 -1l7 0l6 0l5 0l5 0l4 5l3 5l1 8l1 8l0 11l-5 25l-6 7l-5 4l-5 2l-9 5l-6 1l-3 1l-4 0l-4 0l-3 0l-1 -2l-2 -2l0 -2l0 -4l0 -2l0 -2l0 -1l1 -2l4 -3l2 -2l2 -1l4 -2l3 -1l2 -2l2 -1";
+    int strokeWidth;
 
     public TestView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        float density = getResources().getDisplayMetrics().densityDpi;
+        strokeWidth = (int)(density/60f);
+
     }
 
-    public TestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-    public TestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        if (!isReady()) return;
 
-        String svg = "M218.5 139.5l0 0l-1 0l-2 0l-1 0l-2 0l-1 0l-1 0l-2 0l-1 0l-1 0l-1 0l2 -1l4 -2l5 -2l4 -2l11 -4l10 -1l10 -3l8 0l11 0l6 0l6 0l5 2l5 7l3 7l2 8l2 8l1 10l0 9l0 10l-4 8l-6 9l-9 10l-9 6l-9 6l-4 1l-8 4l-13 4l-8 1l-5 0l-5 0l-5 -3l-2 -4l-2 -8l-1 -7l0 -10l0 -8l0 -9l6 -9l5 -7l7 -6l4 -1l11 -7l6 -1l7 0l6 0l5 0l5 0l4 5l3 5l1 8l1 8l0 11l-5 25l-6 7l-5 4l-5 2l-9 5l-6 1l-3 1l-4 0l-4 0l-3 0l-1 -2l-2 -2l0 -2l0 -4l0 -2l0 -2l0 -1l1 -2l4 -3l2 -2l2 -1l4 -2l3 -1l2 -2l2 -1";
-
-        paint = new Paint();
-        paint.setStrokeWidth(5);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.BLACK);
-        canvas.drawPath(svgToPath(svg),paint);
-    }
-
-    Path svgToPath(String svg){
-        String[] svgs = svg.split("l");
         Path path = new Path();
+        String[] svgs = svg.split("l");
         String[] hoge = svgs[0].substring(1).split(" ");
         float from = Float.parseFloat(hoge[0]);
         float to = Float.parseFloat(hoge[1]);
-        path.moveTo(from,to);
-        Log.d("hoge", String.valueOf(from) + to);
+        PointF pointF = sourceToViewCoord(from,to);
+        path.moveTo(pointF.x,pointF.y);
         for (int i = 1;i < svgs.length;i++){
-            Log.d("auhguiawef", svgs[i]);
             String[] lhoge = svgs[i].split(" ");
             float lfrom = Float.parseFloat(lhoge[0]);
             float lto = Float.parseFloat(lhoge[1]);
             path.rLineTo(lfrom,lto);
         }
-        return path;
+
+        Paint paint = new Paint();
+
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeWidth(strokeWidth * 2);
+        paint.setColor(Color.BLACK);
+
+        canvas.drawPath(path,paint);
     }
+
+
 }
